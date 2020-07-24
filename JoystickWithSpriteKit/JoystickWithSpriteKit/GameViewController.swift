b0c03853522b340aeffa9a82ea8a631b7ebe1e76
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-07-24 13:27:06 +0300

    Add files via upload

---
+++ JoystickWithSpriteKit/JoystickWithSpriteKit/GameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GameViewController.swift
+//  JoystickWithSpriteKit
+//
+//  Created by Furkan Hanci on 7/10/20.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: self.view.bounds.size)
+        scene.backgroundColor = .white
+        
+        if let skView = self.view as? SKView {
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.ignoresSiblingOrder = true
+            skView.presentScene(scene)
+        }
+    }
+    
+    override var shouldAutorotate : Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask  {
+        return UIDevice.current.userInterfaceIdiom == .phone ? .allButUpsideDown : .all
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
